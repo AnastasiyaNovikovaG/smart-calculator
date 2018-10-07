eb6f520ac9f94afb82bb33181042125359ba6e6b
nasti <AnastasiyaNovikovaG>
2018-10-07 22:40:59 +0300

    answer

--- src/index.js
+++ src/index.js
@@ -1,27 +1,83 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this.result = initialValue;
+        this.mass = [initialValue];
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    add(number) {
+        this.mass.push("+");
+        this.mass.push(number);
+        return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    subtract(number) {
+        this.mass.push("-");
+        this.mass.push(number);
+        return this;
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    multiply(number) {
+        this.mass.push("*");
+        this.mass.push(number);
+        return this;
+    }
+
+    devide(number) {
+        this.mass.push("/");
+        this.mass.push(number);
+        return this;
+    }
+
+    pow(number) {
+        this.mass.push("^");
+        this.mass.push(number);
+        return this;
+    }
+
+    valueOf() {
+
+        let massFin = this.mass;
+        let length = this.mass.length;
+        for (let i = length; i > 0; i--) {
+            if (massFin[i] === "^") {
+                let val = Math.pow(massFin[i - 1], massFin[i + 1])
+                massFin.splice(i - 1, 3, val);
+                length = massFin.length;
+                i = length;
+            }
+        }
+
+        for (let i = 0; i < length; i++) {
+            if (massFin[i] === "*") {
+                let val = massFin[i - 1] * massFin[i + 1];
+                massFin.splice(i - 1, 3, val);
+                i = 0;
+                length = massFin.length;
+            } else if (massFin[i] === "/") {
+                let val = massFin[i - 1] / massFin[i + 1];
+                massFin.splice(i - 1, 3, val);
+                i = 0;
+                length = massFin.length;
+            }
+        }
+
+        for (let i = 0; i < length; i++) {
+            if (massFin[i] === "+") {
+                let val = (+massFin[i - 1]) + (+massFin[i + 1]);
+                massFin.splice(i - 1, 3, val);
+                i = 0;
+                length = massFin.length;
+            } else if (massFin[i] === "-") {
+                let val = massFin[i - 1] - massFin[i + 1];
+                massFin.splice(i - 1, 3, val);
+                i = 0;
+                length = massFin.length;
+            }
+        }
+        return massFin[0];
+    }
 }
+
 
 module.exports = SmartCalculator;
